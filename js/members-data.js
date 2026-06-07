287d55ca11e47374f9d60e36869426f430f844df
光明与尘埃 <guangmingyuchenai@163.com>
2026-06-07 10:45:08 +0000

    尝试使用members-data.js

---
+++ js/members-data.js
@@ -0,0 +1,33 @@
+// js/members-data.js
+const MEMBERS = [
+    {
+        name: "老魏",
+        avatar: "/images/members/wei.jpeg",
+        bio: "公社社长、硬算社社长",
+        subgroup: "yingsuan"
+    },
+    {
+        name: "向死而生",
+        avatar: "/images/members/xses.jpeg",
+        bio: "公社常务委员会委员长、朗诵社社长、宣传委员会委员长",
+        subgroup: "langsong"
+    },
+    {
+        name: "光明与尘埃",
+        avatar: "/images/members/gmyca.jpeg",
+        bio: "计算机科学社社长、官网搭建与维护者",
+        subgroup: "cs"
+    },
+    {
+        name: "C0",
+        avatar: "/images/members/C0.jpg",
+        bio: "计算机科学社社员、官网优化与维护者",
+        subgroup: "cs"
+    },
+    {
+        name: "qian",
+        avatar: "/images/members/qian.jpeg",
+        bio: "联合公社二办事处处长和文件编写员，朗诵社社员",
+        subgroup: "langsong"
+    }
+];
